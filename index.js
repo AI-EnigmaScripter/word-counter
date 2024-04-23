522dd55c8670c04d0a52089dbd178f1f2a418237
Ghulam Fareed <faisalnathani128@gmail.com>
2024-04-23 05:57:59 -0700

    Initial commit for main branch

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+async function countWordsAndCharacters() {
+    const userInput = await inquirer.prompt({
+        type: 'input',
+        name: 'paragraph',
+        message: 'Enter a paragraph:',
+    });
+    const paragraph = userInput.paragraph;
+    const characters = paragraph.replace(/\s+/g, '').split(''); // Split the paragraph into single characters
+    const formattedCharacters = characters.map(char => `[ '${char}' ]`).join(' '); // each character in square brackets and single quotes, then join them with space
+    console.log(chalk.green('Formatted paragraph:'));
+    console.log(chalk.yellow(formattedCharacters)); // Print the formatted paragraph
+    const totalCharCount = characters.length; // Get the total character count of the paragraph
+    console.log(chalk.green('Total character count:'), totalCharCount);
+}
+countWordsAndCharacters();
